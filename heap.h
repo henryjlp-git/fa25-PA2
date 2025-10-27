9876cb8b689f2b08d714047c0b0f803589e7594f
Henry Parubrub <hparubrub1794@sdsu.edu>
2025-10-27 16:25:33 -0700

    Implemented push and pop in heap.h

--- heap.h
+++ heap.h
@@ -16,12 +16,20 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        data[size] = idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) return -1;
+        int ret = data[0];
+        data[0] = data[size - 1];
+        size--;
+        if (size > 0) downheap(0, weightArr);
+        return ret;
     }
 
     void upheap(int pos, int weightArr[]) {
